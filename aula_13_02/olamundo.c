b79fbf42e86083fc6a8c86e5642d1faf83a35067
Helena Hagsma <helenahagsma2024@gmail.com>
2026-02-20 21:46:36 -0300

    Enhance olamundo.c with explanatory comments
    Added comments explaining the C program structure and functionality.

--- aula_13_02/olamundo.c
+++ aula_13_02/olamundo.c
@@ -1,6 +1,8 @@
-#include <stdio.h>
+/*-------------------Primeiro programa em C*/
 
-int main(){
-    printf("Ola Mundo!");
-    return 0;
+#include <stdio.h> /*é uma diretiva de compilação, em que pede para o compilador inserir a biblioteca padrão de entrada e saída de dados*/
+
+int main(){ /*o compilador executa a leitura pelo método main() retornando 0 quando a saída foi satisfatória*/
+    printf("Ola Mundo!"); /*imprimir um dado na tela*/
+    return 0; 
 }
