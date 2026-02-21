25941ace6cf88a9e9bf5ba9975f82dbcd620434f
Helena Hagsma <helenahagsma2024@gmail.com>
2026-02-20 22:32:26 -0300

    Add files via upload

---
+++ aula_20_02/desenhos_no_terminal.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(){
+    printf("     *\n   * * *\n  * * * *\n * * * * *\n    |||\n\tA christmas tree!\n\n");
+    return 0;
+}
---
+++ aula_20_02/exemplo_variaveis.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    int a = 5;
+    int b = 10;
+    int soma = a + b;
+    printf("%d", soma);
+    return 0;
+}
