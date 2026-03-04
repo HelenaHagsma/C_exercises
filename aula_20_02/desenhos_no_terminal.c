ea586fb7fbad30290fbd0deacbb68177a266cc48
Helena Hagsma <helenahagsma2024@gmail.com>
2026-03-04 14:31:02 -0300

    new exercises

--- aula_20_02/desenhos_no_terminal.c
+++ aula_20_02/desenhos_no_terminal.c
@@ -2,5 +2,7 @@
 
 int main(){
     printf("     *\n   * * *\n  * * * *\n * * * * *\n    |||\n\tA christmas tree!\n\n");
+    printf("    /\\\n   /  \\\n  /____\\\n  |    |\n  |____|\n\tA house!\n\n");
+    printf(" /\\__/\\\n(  o.o )\n  > ^ < \n\tA cat!");
     return 0;
 }
