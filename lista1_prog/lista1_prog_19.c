ee9d60435416732b76f238e1ceb8ad2afe93d568
Helena Hagsma <helenahagsma2024@gmail.com>
2026-03-13 22:14:02 -0300

    Refactor digit extraction logic in main function

--- lista1_prog/lista1_prog_19.c
+++ lista1_prog/lista1_prog_19.c
@@ -5,9 +5,10 @@ int main() {
     printf("Digite um numero inteiro positivo menor que 1000: ");
     scanf("%d", &n);
 
-    uni = n % 10;
-    dez = (n / 10) % 10; 
-    cen = n / 100; 
+    cen = n / 100;
+    dez = (n % 100)/10;
+    uni = (n % 10); 
+
     soma = uni + dez + cen;
     
     printf("A soma dos digitos %d + %d + %d e: %d\n",cen, dez, uni, soma);
