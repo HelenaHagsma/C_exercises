439ad17aadc0726ea920b16d7f42cf81207d9723
Helena Hagsma <helenahagsma2024@gmail.com>
2026-03-17 08:26:33 -0300

    Calculate distance between two points in 2D space

--- lista1_prog/lista1_prog_30.c
+++ lista1_prog/lista1_prog_30.c
@@ -1,14 +1,16 @@
-#include <stdio.h>
+#include <math.h>
 
 int main(){
-    float p1, p2, d;
+    double x1, x2, y1, y2, d, p1, p2;
     printf("Informe o primeiro ponto: ");
-    scanf("%f", &p1);
+    scanf("%lf %lf", &x1, &y1);
     printf("Informe o segundo ponto: ");
-    scanf("%f", &p2);
+    scanf("%lf %lf", &x2, &y2);
 
-    d = p1 - p2;
+    p1 = pow(x2 - x1, 2);
+    p2 = pow(y2 - y1, 2);
+    d = sqrt(p1 + p2);
     
-    printf("A distancia entre os dois pontos e: %.2f", d);
+    printf("A distancia entre os dois pontos e: %.2lf", d);
     return 0;
 }
